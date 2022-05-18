27e1708e8ef9ab48d4bb2dbecf2e10fc28048a87
it21240942 <99234907+it21240942@users.noreply.github.com>
2022-05-19 02:52:25 +0530

    Create FeedBack.cpp

---
+++ FeedBack.cpp
@@ -0,0 +1,20 @@
+//IT21240942
+#include<iostream>
+
+//FeedBack implementation
+    FeedBack :: FeedBack()
+    {
+
+    }
+    void FeedBack::addFeedBack()
+    {
+
+    }
+    void FeedBack::displayFeedBack()
+    {
+
+    }
+   FeedBack ::  ~FeedBack()
+    {
+
+    }
