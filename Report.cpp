e444197e97c8a69d8e93cf8c717fb8f2781b7cb5
IT21240706 <99425401+IT21240706@users.noreply.github.com>
2022-05-19 02:57:33 +0530

    Create Report.cpp

---
+++ Report.cpp
@@ -0,0 +1,26 @@
+/*------IT21240706-----*/
+//Report Implementation
+
+#include <iostream>
+#include "Report.h"
+#include <cstring>
+Report :: Report()
+{
+
+}
+Report :: Report(int rID, const char rName[])
+{
+
+}
+void Report :: addReportAdmin() 
+{
+
+}
+void Report :: displayReportDetails()
+{
+
+}
+Report :: ~Report()
+{
+
+}
