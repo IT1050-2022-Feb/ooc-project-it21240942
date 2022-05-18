65708b31e1508e9bb1190292d3b15d9d2fc1f5af
it21240942 <99234907+it21240942@users.noreply.github.com>
2022-05-19 02:50:21 +0530

    Create FeedBack.h

---
+++ FeedBack.h
@@ -0,0 +1,16 @@
+//IT21240942
+#include<iostream>
+//FeedBack.h
+class FeedBack
+{
+private:
+    int feedBackNo;
+    string feedBackMsg;
+public:
+
+    FeedBack();
+    void addFeedBack();
+    void displayFeedBack();
+    ~FeedBack();
+
+};
